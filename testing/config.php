19122179b94f10e0e850efef797f0348d7b873ae
pashaessel <essel071@yandex.ru>
2022-03-22 21:38:27 +0300

    init

--- testing/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", "localhost");
-define("USER", "root");
-define("PASS", "");
-define("DB", "testing");
-
-$db = @mysqli_connect(HOST, USER, PASS, DB) or die('Нет соединения БД'); //@ для того, чтобы выводилась только эта ошибка
-mysqli_set_charset($db, 'utf8') or die('Не установлена кодировка соединения ');
-?>
